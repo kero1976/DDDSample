43e28de35c6452a82937f1879af3b9bb470714ce
kero1976 <kero1976@outlook.jp>
2020-03-28 09:09:40 +0900

    プロジェクト ファイルを追加します。

---
+++ DDDSample.Infrastructure/src/Fake/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDSample.Infrastructure.src.Fake
+{
+    class Class1
+    {
+    }
+}
